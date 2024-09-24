e6eba5814f10d34a95880ca85761b88a0b15b520
killflex <ferryhasanbackup@gmail.com>
2024-09-25 00:18:23 +0700

    initial commit

---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>killflex's portfolio</title>
+    <link href="<?= base_url().'assets/css/output.css'?>" rel="stylesheet" >
+</head>
+<body>
+    <h1 class="text-">dawdwad</h1>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./application/views/**/*.{html, php}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
